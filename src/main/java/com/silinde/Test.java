0527380b5f006fd67f9cdd199b954d9c9d139d5c
Pau Rodriguez Molina <65486507+Silinde87@users.noreply.github.com>
2020-07-02 18:04:05 +0200

    Update Test.java

--- src/main/java/com/silinde/Test.java
+++ src/main/java/com/silinde/Test.java
@@ -9,7 +9,17 @@ public class Test {
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
-            telegramBotsApi.registerBot(new TelegramBot());
+        	telegramBotsApi.registerBot(new TelegramBot());
+		JFrame frame = new JFrame("Hangman Game");
+        	JPanel panel = new JPanel();
+        	panel.setLayout(new FlowLayout());
+        	JLabel label = new JLabel("Game is running. Close to exit.");        	   	
+        	panel.add(label);        	
+        	frame.add(panel);
+        	frame.setSize(300, 100);
+        	frame.setLocationRelativeTo(null);
+        	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        	frame.setVisible(true); 
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
