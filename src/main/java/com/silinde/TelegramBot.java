f303e68a16606e95424d712b3c041fa9b31db5de
Pau Rodriguez Molina <65486507+Silinde87@users.noreply.github.com>
2020-07-14 10:39:28 +0200

    Add files via upload

--- src/main/java/com/silinde/TelegramBot.java
+++ src/main/java/com/silinde/TelegramBot.java
@@ -19,11 +19,17 @@ public class TelegramBot extends TelegramLongPollingBot{
 	public void onUpdateReceived(Update update) {		
 		Long chatID = update.getMessage().getChatId();
 		Integer userID = update.getMessage().getFrom().getId();
+		StringBuilder mess = new StringBuilder("");
+		int chInPlay = 0;
+		
+		//Bot is used from Telegram group condition
+		if (update.getMessage().isGroupMessage())
+			chInPlay = 1;
 		
 		//Detecting commands
-		switch (update.getMessage().getText().toLowerCase()) {
+		switch (update.getMessage().getText().toLowerCase()) {			
 			case "/help":
-				sendM("¡Hola " + update.getMessage().getFrom().getFirstName() + "!" + 
+				sendM("Â¡Hola " + update.getMessage().getFrom().getFirstName() + "!" + 
 						"\n/start - Iniciar un nuevo juego" +
 						"\n/exit - Borrar partida actual",chatID);
 				break;
@@ -31,7 +37,7 @@ public class TelegramBot extends TelegramLongPollingBot{
 			case "/exit":
 				if(userList.containsKey(userID)) {
 					userList.remove(userID);
-					sendM("Partida borrada con éxito.\nEscribe /start para iniciar una nueva.",chatID);
+					sendM("Partida borrada con Ã©xito.\nEscribe /start para iniciar una nueva.",chatID);
 				}else {
 					sendM(update.getMessage().getFrom().getFirstName() + ", no has iniciado ninguna partida.\n"
 							+ "Escribe /start para iniciar una nueva.",chatID);					
@@ -40,72 +46,67 @@ public class TelegramBot extends TelegramLongPollingBot{
 
 			//HangMangame Case
 			case "/start":
-				//User exists on Map userList
-				if(userList.containsKey(userID)) {
-					sendM("¡No dejes colgado al cactus!",chatID);
-					if(((hg.MAX_ATTEMPTS) - userList.get(userID).attempts) > 1 )
-						sendM("Te quedan " + ((hg.MAX_ATTEMPTS) - 
-								userList.get(userID).attempts) + " intentos.",chatID);	
-					else 						
-						sendM("Te queda " + ((hg.MAX_ATTEMPTS) - 
-								userList.get(userID).attempts) + " intento.",chatID);				
-					sendM(printWord(userID),chatID);
-					sendM("Escribe una letra...",chatID);
+				//User exists on Map userList, show warning message. Still in play..
+				if(userList.containsKey(userID)) {					
+					mess.append(update.getMessage().getFrom().getFirstName() + ", Â¡No dejes colgado al cactus!");					
+					attLeft(userID, mess, chatID);					
+					sendM("\n\nEscribe una letra...",chatID);
 					break;
 				}else {
+					//User doesn't exists on Map userList. Game begins...
 					hg = new HangmanGame(userID,chatID);				
 					//Storing user id
 					userList.put(userID, hg);				
-					sendM("| Bienvenido al clásico juego del ahorcado |",chatID);
-					sendM("¿Serás capaz de descubrir la siguiente palabra?\n" + printWord(userID),chatID);
-					sendM("Escribe una letra...",chatID);	
+					sendM("| Bienvenido al juego del ahorcado |\n"
+							+ "Â¿SerÃ¡s capaz de descubrir la palabra?\n" + printWord(userID) + 
+							"\n\n" + update.getMessage().getFrom().getFirstName() + ", escribe una letra...",chatID);					
 					break;	
 				}
 				
 			default:
+				//Default case is used for unknown commands or letters when the game is started.
+				//Game is started
 				if (userList.get(userID).hangmanIsStarted) {
 					//Game is alive condition.
 					if((hg.MAX_ATTEMPTS - userList.get(userID).attempts)>0 &&
 							userList.get(userID).rightChars != userList.get(userID).totalChars) {
 						userList.get(userID).charInPlay = update.getMessage().getText().trim().toUpperCase();
 						
+						
 						//Check valid input options
-						if(userList.get(userID).charInPlay.length()>1 || 
-								userList.get(userID).charInPlay.isEmpty() ||
-								userList.get(userID).charInPlay.matches(hg.REGEX)) {	
-							sendM("Debes escribir una sola letra. Prueba de nuevo...",chatID);
-							return;									
-						}
+						if((userList.get(userID).charInPlay.isEmpty() || userList.get(userID).charInPlay.matches(hg.REGEX)) ||
+								(chInPlay==0 && userList.get(userID).charInPlay.length()>1) ||
+								(chInPlay==1 && userList.get(userID).charInPlay.length()>2)) {
+							sendM(update.getMessage().getFrom().getFirstName() + ", debes escribir una sola letra. Prueba de nuevo...",chatID);
+							return;
+						}						
+					
 						//Checking result of selected-by-user valid input character.
-						switch (matchingChar(userID, userList.get(userID).charInPlay.charAt(0))) {
+						switch (matchingChar(userID, userList.get(userID).charInPlay.charAt(chInPlay))) {
 							case 1:
-								//Case repeating letter
-								sendM("¡FALLO! La letra " + userList.get(userID).charInPlay.charAt(0) + " está repetida",chatID);
-								if((hg.MAX_ATTEMPTS - userList.get(userID).attempts)>1)
-									sendM("Te quedan " + (hg.MAX_ATTEMPTS - userList.get(userID).attempts) + " intentos.",chatID);	
-								else {
-									if ((hg.MAX_ATTEMPTS - userList.get(userID).attempts)==0)						
-										break;							
-									sendM("Te queda " + (hg.MAX_ATTEMPTS - userList.get(userID).attempts) + " intento.",chatID);
-								}
-								sendPh(userID, chatID,(hg.MAX_ATTEMPTS - userList.get(userID).attempts));						
+								//Case repeated letter
+								mess.append("Â¡FALLO!\nLa letra " + userList.get(userID).charInPlay.charAt(chInPlay) + " estÃ¡ repetida");								
+								if ((hg.MAX_ATTEMPTS - userList.get(userID).attempts)==0)						
+									break;	
+								attLeft(userID, mess, chatID);		
+								sendPh(userID, chatID,(hg.MAX_ATTEMPTS - userList.get(userID).attempts));
 								break;
 							case 2:
 								//case right letter
-								sendM("¡ACIERTO! La letra " + userList.get(userID).charInPlay.charAt(0) + " ha sido encontrada.",chatID);
-								if((userList.get(userID).totalChars - userList.get(userID).rightChars == 1))
+								if((userList.get(userID).totalChars - userList.get(userID).rightChars == 1)) {
 									userList.get(userID).rightChars++;
-								break;
+									break;
+								}else {
+									mess.append("Â¡ACIERTO!\nLa letra " + userList.get(userID).charInPlay.charAt(chInPlay) + " ha sido encontrada.\n");
+									break;
+								}
+
 							case 3:
 								//case wrong letter
-								sendM("¡FALLO! La letra " + userList.get(userID).charInPlay.charAt(0) + " no ha sido encontrada.",chatID);
-								if((hg.MAX_ATTEMPTS - userList.get(userID).attempts)>1)
-									sendM("Te quedan " + (hg.MAX_ATTEMPTS - userList.get(userID).attempts) + " intentos.",chatID);	
-								else {
-									if ((hg.MAX_ATTEMPTS - userList.get(userID).attempts)==0)
-										break;
-									sendM("Te queda " + (hg.MAX_ATTEMPTS - userList.get(userID).attempts) + " intento.",chatID);
-								}
+								mess.append("Â¡FALLO!\nLa letra " + userList.get(userID).charInPlay.charAt(chInPlay) + " no ha sido encontrada.");		
+								if ((hg.MAX_ATTEMPTS - userList.get(userID).attempts)==0)						
+									break;	
+								attLeft(userID, mess, chatID);								
 								sendPh(userID, chatID,(hg.MAX_ATTEMPTS - userList.get(userID).attempts));
 								break;						
 							}
@@ -113,29 +114,45 @@ public class TelegramBot extends TelegramLongPollingBot{
 								(userList.get(userID).totalChars - userList.get(userID).rightChars) == 0) {							
 							//Check final score
 							if ((userList.get(userID).totalChars - userList.get(userID).rightChars) == 0) {
-								sendM("¡HAS GANADO! La palabra es: \n" + printWord(userID),chatID);
+								mess.append("Â¡HAS GANADO!\nLa palabra es: " + printWord(userID) + "\n\nEscribe /start para jugar de nuevo.");
 								sendPh(userID, chatID,22);
+								sendM(mess.toString(),chatID);
 								userList.get(userID).hangmanIsStarted = false;
 								userList.remove(userID);
-								sendM("Escribe /start para jugar de nuevo.",chatID);
 							}else if((hg.MAX_ATTEMPTS - userList.get(userID).attempts)==0){
 								sendPh(userID, chatID,(hg.MAX_ATTEMPTS - userList.get(userID).attempts));
 								for (char c : userList.get(userID).word.toCharArray())
 									userList.get(userID).letters.replace(c, true);												
-								sendM("¡HAS SIDO DERROTADO! La palabra era: \n" + printWord(userID),chatID);							
+								sendM("Â¡HAS SIDO DERROTADO!\nLa palabra era: " + printWord(userID) + "\n\nEscribe /start para jugar de nuevo.",chatID);							
 								userList.get(userID).hangmanIsStarted = false;
-								userList.remove(userID);
-								sendM("Escribe /start para jugar de nuevo.",chatID);
+								userList.remove(userID);								
 							}
 							break;
 						}
-						sendM(printWord(userID),chatID);
-						sendM("Escribe una letra...",chatID);
+						mess.append(printWord(userID) + "\n\n" + update.getMessage().getFrom().getFirstName() + ", escribe una letra...");
+						sendM(mess.toString(),chatID);
+						mess.setLength(0);						
 					}
+				//Game is NOT started
 				}else
-					sendM(update.getMessage().getFrom().getFirstName() + ", has elegido una opción no existente",chatID);						
-		}			
+					sendM(update.getMessage().getFrom().getFirstName() + ", has elegido una opciÃ³n no existente",chatID);						
+		}
 	}
+	
+	
+	//This method shows the attempts left
+	public void attLeft(int uID, StringBuilder ms, long cID) {		
+		if(((hg.MAX_ATTEMPTS) - userList.get(uID).attempts) > 1 ) {
+			ms.append("\nTe quedan " + (hg.MAX_ATTEMPTS - userList.get(uID).attempts) + " intentos.");
+			sendM(ms.toString(),cID);
+			ms.setLength(0);
+		}else {
+			ms.append("\nTe queda " + (hg.MAX_ATTEMPTS - userList.get(uID).attempts) + " intento.");
+			sendM(ms.toString(),cID);
+			ms.setLength(0);
+		}
+	}
+
 	
 	//This method prints the word with blank spaces while the game works.
 	public String printWord(int uID) {
